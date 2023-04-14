ebc35e6bee8a183206369526f6847ddb3b678165
Łukasz Głogowski <lukaszglogowski98@gmail.com>
2023-04-14 23:23:45 +0200

    Added JSON & comments for clarity

--- app/src/main/java/com/example/ppiwd/MainActivity.java
+++ app/src/main/java/com/example/ppiwd/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ppiwd;
 
 
+import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
@@ -9,9 +10,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
+
+
 
 import com.mbientlab.metawear.Data;
 import com.mbientlab.metawear.MetaWearBoard;
@@ -23,20 +27,46 @@ import com.mbientlab.metawear.builder.RouteComponent;
 import com.mbientlab.metawear.data.Acceleration;
 import com.mbientlab.metawear.data.AngularVelocity;
 import com.mbientlab.metawear.module.Accelerometer;
-import com.mbientlab.metawear.module.Gyro;
 import com.mbientlab.metawear.module.GyroBmi160;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import bolts.Continuation;
 import bolts.Task;
 
+
+/*   !!!!!!!!!!! WAŻNE !!!!!!!!!!!!!
+* 1. Jeśli apka odmawia połączenia się z czujnikiem, trzeba wejść do ustawień
+* Bluetooth w telefonie i sparować telefon z czujnikiem, problemy powinny ustąpić.
+* 2. Po odkomentowaniu kodu w "writeToJson" apka będzie krzyczeć o braku pozoleń,
+* trzeba wejść w ustawienia apki w ustawieniach telefonu i dać jej dostęp do pamięci. */
+
+
+
 public class MainActivity extends Activity implements ServiceConnection {
 
     private BtleService.LocalBinder serviceBinder;
-    ///< Address MAC of your device
+    //< Adres MAC urządzenia, żeby korzystać z własnego trzeba go zmienić
     private final String MW_MAC_ADDRESS= "E1:15:73:BF:22:D1";
+    //< Deklaracje akcelerometru i żyroskopu
     private MetaWearBoard board;
     private Accelerometer accelerometer;
     private GyroBmi160 gyroBmi160;
+    //< Deklaracje obiektów JSONa
+    private JSONObject deviceData;
+    private JSONArray dataArray;
+    private JSONObject dataArray2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +77,7 @@ public class MainActivity extends Activity implements ServiceConnection {
         getApplicationContext().bindService(new Intent(this, BtleService.class),
                 this, Context.BIND_AUTO_CREATE);
 
+        //< Ten przycisk rozpoczyna pomiary
         findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,6 +87,7 @@ public class MainActivity extends Activity implements ServiceConnection {
                 gyroBmi160.start();
             }
         });
+        //< Ten przycisk kończy pomiary
         findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,6 +95,13 @@ public class MainActivity extends Activity implements ServiceConnection {
                 accelerometer.stop();
                 gyroBmi160.angularVelocity().stop();
                 accelerometer.acceleration().stop();
+            }
+        });
+        //< Ten przycisk zapisuje pomiary do JSONa
+        findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                writeToJson();
             }
         });
     }
@@ -90,11 +129,24 @@ public class MainActivity extends Activity implements ServiceConnection {
 
     }
 
+    //< Główna część apki, pobiera dane z czujnika i zapisuje do JSONa
     public void retrieveBoard(String macAdress) {
         final BluetoothManager btManager=
                 (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         final BluetoothDevice remoteDevice=
                 btManager.getAdapter().getRemoteDevice(macAdress);
+
+        //< Inicjalizacja obiektów do JSONa
+        deviceData = new JSONObject();
+        dataArray = new JSONArray();
+        dataArray2 = new JSONObject();
+
+        //< Adres MAC czujnika do JSONa
+        try {
+            deviceData.put("device", MW_MAC_ADDRESS);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         // Create a MetaWear board object for the Bluetooth Device
         board = serviceBinder.getMetaWearBoard(remoteDevice);
@@ -102,15 +154,17 @@ public class MainActivity extends Activity implements ServiceConnection {
         board.connectAsync().onSuccessTask(new Continuation<Void, Task<Route>>() {
             @Override
             public Task<Route> then(Task<Void> task) throws Exception {
+                //< Sprawdzanie czy telefon i czujnik się połączyły
                 if (task.isFaulted()) {
                     Log.i("ppiwd", "Failed to connect");
                 } else {
                     Log.i("ppiwd", "Connected to " + macAdress);
                 }
 
+                //< Zostawiłem 25Hz bo to dane i tak szybko lecą, można zmienić
                 accelerometer = board.getModule(Accelerometer.class);
                 accelerometer.configure()
-                        .odr(50f)       // Set sampling frequency to 25Hz, or closest valid ODR
+                        .odr(25f)       // Set sampling frequency to 25Hz, or closest valid ODR
                         .commit();
                 return accelerometer.acceleration().addRouteAsync(new RouteBuilder() {
                     @Override
@@ -118,7 +172,19 @@ public class MainActivity extends Activity implements ServiceConnection {
                         source.stream(new Subscriber() {
                             @Override
                             public void apply(Data data, Object... env) {
-                                Log.i("ppiwd", "Acce: " + data.value(Acceleration.class).toString());
+                                //< Odkomentować jeśli chce się zobaczyć dane Accel w Logcat
+                                //Log.i("ppiwd", "Accel: " + data.value(Acceleration.class).toString());
+
+                                try {
+                                    JSONArray accel = new JSONArray();
+                                    accel.put(data.value(Acceleration.class).toString());
+
+                                    //< Dane z Accel do JSONa
+                                    // Accel trochę się tnie i nie ma go w pierwszych 2-3 obiektach, potem ok
+                                    dataArray2.put("accel", accel);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
                             }
                         });
                     }
@@ -138,9 +204,10 @@ public class MainActivity extends Activity implements ServiceConnection {
         }).continueWith(new Continuation<Void, Task<Route>>() {
             @Override
             public Task<Route> then(Task<Void> task) throws Exception {
+                //< Tutaj też zostawiłem 25Hz, zmienić na ODR_50_HZ albo coś innego
                 gyroBmi160 = board.getModule(GyroBmi160.class);
                 gyroBmi160.configure()
-                        .odr(GyroBmi160.OutputDataRate.ODR_50_HZ)
+                        .odr(GyroBmi160.OutputDataRate.ODR_25_HZ)
                         .commit();
                 return gyroBmi160.angularVelocity().addRouteAsync(new RouteBuilder() {
                     @Override
@@ -148,21 +215,73 @@ public class MainActivity extends Activity implements ServiceConnection {
                         routeComponent.stream(new Subscriber() {
                             @Override
                             public void apply(Data data, Object... env) {
-                                Log.i("ppiwd", "Gyro:" + data.value(AngularVelocity.class).toString());
+                                //< Odkomentować jeśli chce się zobaczyć dane Gyro w Logcat
+                                //Log.i("ppiwd", "Gyro:" + data.value(AngularVelocity.class).toString());
+
+                                try {
+                                    Long tsLong = System.currentTimeMillis();
+                                    String ts = tsLong.toString();
+                                    JSONArray gyro = new JSONArray();
+
+                                    //< Dane z timestamp i Gyro do JSONa
+                                    gyro.put(data.value(AngularVelocity.class).toString());
+                                    dataArray2.put("timestamp", ts);
+                                    dataArray2.put("gyro", gyro);
+                                    dataArray.put(dataArray2);
+                                    deviceData.put("data", dataArray);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+
+                                //< Skończony JSON w pełnej glorii i chwale, w Logcat
+                                Log.i("ppiwd", deviceData.toString());
                             }
                         });
                     }
                 });
             }
         });
+    }
 
-        // THIS SHOULD ALWAYS STAY COMMENTED
-        /*board.disconnectAsync().continueWith(new Continuation<Void, Void>() {
-            @Override
-            public Void then(Task<Void> task) throws Exception {
-                Log.i("ppiwd", "Disconnected");
-                return null;
+    /*< Zapisywanie JSONa do pamięci telefonu, niestety z automatu idzie do wewnętrznej
+    pamięci aplikacji i nie da się go tam podejrzeć ani z poziomu telefonu dostać do niego.
+    Zakomentowany kod kopiuje plik do pamięci zewnętrzej telefonu ale coś jest z nim nie
+    tak, bo ciągle powtaża się pierwszy obiekt. */
+    public void writeToJson() {
+        final String filename = "data.json";
+        String fileContents = deviceData.toString();
+        FileOutputStream outputStream;
+
+        /*File sourceLocation = new File("/data/data/com.example.ppiwd/files/data.json");
+        String sdCard = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File targetLocation = new File(sdCard, "data.json");
+
+        Log.v("ppiwd", "sourceLocation: " + sourceLocation);
+        Log.v("ppiwd", "targetLocation: " + targetLocation);*/
+
+        //< Zapis JSONa do pamięci wewnętrznej apki
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(fileContents.getBytes());
+            outputStream.close();
+            Log.i("ppiwd", "File saved: " + filename);
+
+            /*InputStream in = new FileInputStream(sourceLocation);
+            OutputStream out = new FileOutputStream(targetLocation);
+
+            // Copy the bits from instream to outstream
+            byte[] buf = new byte[1024];
+            int len;
+
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
             }
-        });*/
+
+            in.close();
+            out.close();*/
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
